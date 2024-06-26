a7fe3b9178f24ffeda7b21c8ed7be1744904a6d1
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-26 19:43:55 +0200

    Create Utils.java
    added faiNuotare() and faiVolare()

---
+++ src/main/java/org/exercise/java/Utils.java
@@ -0,0 +1,15 @@
+package org.exercise.java;
+
+public class Utils {
+
+	public static void faiVolare(IVolante animale) {
+		animale.vola();
+
+	}
+
+	public static void faiNuotare(INuotante animale) {
+		animale.nuota();
+
+	}
+
+}
